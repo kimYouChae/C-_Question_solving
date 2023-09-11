4688d39990fcb4c6cd71dea2e16ca15ed4de4c9c
youcheae <kjhstar2486@naver.com>
2023-09-11 18:18:05 +0900

    boj_9655
    9655 : DP (라 읽고 홀수짝수 판별이라 쓴다)

---
+++ boj/boj_9655.cpp
@@ -0,0 +1,37 @@
+
+#include <sstream> //split() , stringstream
+#include <list>
+#include <deque>
+#include <map>
+#include <stack>
+#include<cmath> // math.pow
+using namespace std;
+#include <iostream>
+#include <vector>
+#include <set>
+#include <queue>
+#include <algorithm> // sort
+#include <string.h> //memset
+#include <string> //stoi()
+#include<tuple> // turple
+
+int N, M;
+
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	//น้มุ 9655 
+	cin >> N;
+	if (N % 2 == 0)
+	{
+		cout << "CY";
+	}
+	else
+	{
+		cout << "SK";
+	}
+
+}
