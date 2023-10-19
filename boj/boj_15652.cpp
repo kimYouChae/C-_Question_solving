d4ea2b9888bb2ccab9a89662acd23b2965f2546e
youcheae <kjhstar2486@naver.com>
2023-10-19 17:03:14 +0900

    boj_15652
    15652 : 백트래킹

---
+++ boj/boj_15652.cpp
@@ -0,0 +1,54 @@
+
+#include <sstream> //split() , stringstream
+#include <list>
+#include <deque>
+#include <map>
+#include <stack>
+#include<cmath> // math.pow
+using namespace std;
+#include <iostream>
+#include <vector>
+#include <set>
+#include <queue>
+#include <algorithm> // sort
+#include <string.h> //memset
+#include <string> //stoi()
+#include<tuple> // turple
+
+int N, M;
+
+int arr[9];
+bool visit[9];
+void dfs(int num, int cnt)
+{
+	if (cnt == M)
+	{
+		for (int i = 0; i < M; i++)
+		{
+			cout << arr[i] << " ";
+		}
+		cout << '\n';
+		return;
+	}
+
+	for (int i = num; i <= N; i++)
+	{
+		//visit[i] = true;
+		arr[cnt] = i;
+		dfs(i, cnt + 1);
+		//visit[i] = false;
+	}
+
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	// exit(0) : 함수에서 출력하고 바로끝내기
+	// typeid(x).name() : 자료형 출력
+
+	// 백준  
+	cin >> N >> M;
+	dfs(1, 0);
+}
